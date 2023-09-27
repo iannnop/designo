4dfe9b0a44c227733e4b51efa08065ff4a19d7d6
ian <iannnop@gmail.com>
2023-09-26 19:14:57 -0500

    tailwind extend config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "peach": "#E7816B",
+        "light-peach": "#FFAD9B",
+        "black": "#1D1C1E",
+        "dark-gray": "#333136",
+        "light-gray": "#F1F3F5",
+      },
+      backgroundImage: {
+        "footerCTA": "url('../../public/assets/shared/mobile/bg-pattern-design-pages-intro-mobile.svg')",
+        "mobileHomeHero": "url('../../public/assets/home/mobile/bg-pattern-hero-home.svg')",
+        "mobileHero": "url('../../public/assets/shared/mobile/hero-circle.svg')",
+        "smallCircle": "url('../../public/assets/shared/desktop/bg-pattern-small-circle.svg')",
+        "webDesign": "url('../../public/assets/home/mobile/image-web-design.jpg')",
+        "appDesign": "url('../../public/assets/home/mobile/image-app-design.jpg')",
+        "graphicDesign": "url('../../public/assets/home/mobile/image-graphic-design.jpg')",
+      },
       transitionProperty: {
         "width": "width",
         "spacing": 'margin, padding',
@@ -15,7 +31,7 @@ module.exports = {
       },
       screens: {
         "2xsmall": "320px",
-        "xsmall": "512px",
+        "xsmall": "768px",
         "small": "1024px",
         "medium": "1280px",
         "large": "1440px",
@@ -36,5 +52,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@headlessui/tailwindcss'),
+  ],
 }
