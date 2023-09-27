90e3fe784ca1cca7980b67fda1c7625c4cc7d923
ian <iannnop@gmail.com>
2023-09-27 11:56:46 -0500

    hamburger

---
+++ src/modules/common/components/hamburger/index.tsx
@@ -0,0 +1,10 @@
+import clsx from "clsx"
+import React from "react"
+
+const Hamburger = () => {
+  return (
+    <svg width="24" height="20" xmlns="http://www.w3.org/2000/svg"><g fill="#1D1C1E" fillRule="evenodd"><path d="M0 0h24v4H0zM0 8h24v4H0zM0 16h24v4H0z"/></g></svg>
+  )
+}
+
+export default Hamburger
